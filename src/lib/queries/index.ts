aea3c8aab7b09704a18898554f2a2486ccfb7f89
smudgy-g <adamgriff86@gmail.com>
2024-01-15 12:02:21 +0100

    query: get recent posts

--- src/lib/queries/index.ts
+++ src/lib/queries/index.ts
@@ -7,6 +7,7 @@ import {
 import {
   createPost,
   createUserAccount,
+  getRecentPosts,
   signInAccount,
   signOutAccount,
 } from '../appwrite/api'
@@ -25,6 +26,7 @@ export const useSignInAccount = () => {
       signInAccount(user),
   })
 }
+
 export const useSignOutAccount = () => {
   return useMutation({
     mutationFn: () => signOutAccount(),
@@ -40,5 +42,12 @@ export const useCreatePost = () => {
         queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
       })
     },
+  })
+}
+
+export const useGetRecentPosts = () => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+    queryFn: getRecentPosts,
   })
 }
